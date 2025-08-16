fbb2da83216e02d5fe032b68b48798ec0dc81521
moeetomoni <dev@moee.fr>
2025-08-16 21:35:19 +0200

    feat: make the OpenstackService save ratings data in db

--- app/Services/OpenstackService.php
+++ app/Services/OpenstackService.php
@@ -5,45 +5,49 @@ declare(strict_types=1);
 namespace App\Services;
 
 use App\Models\OpenstackCloud;
+use App\Models\OsProject;
+use App\Models\OsRating;
+use App\Models\OsResource;
+use App\Models\User;
 use Carbon\Carbon;
 use Carbon\CarbonPeriod;
 use Illuminate\Support\Facades\Http;
 
 final class OpenstackService
 {
-    public static function isCloudConfigExistForAuth(): bool
+    public static function isCloudConfigExistForAuth(?User $user = null): bool
     {
-        return auth()->user()->openstackClouds()->exists();
+        return $user?->openstackClouds()->exists() ?? auth()->user()->openstackClouds()->exists();
     }
 
-    public function getAccessToken(): string
+    public function getAccessToken(?User $user = null): string
     {
-        if (! self::isCloudConfigExistForAuth()) {
+        if (! self::isCloudConfigExistForAuth($user)) {
             throw new \RuntimeException('No OpenStack cloud configuration found for the user.');
         }
 
-        /** @var ?\App\Models\OpenstackCloud $config */
-        $config = auth()->user()->openstackClouds->first();
+        /** @var ?\App\Models\OpenstackCloud $cloud */
+        $cloud = $user?->openstackClouds->first() ?? auth()->user()->openstackClouds->first();
 
         if (
-            $config->access_token === null
-            || $config->access_token_expires_at === null
-            || $config->access_token_expires_at->isBefore(now())
+            $cloud->access_token === null
+            || $cloud->access_token_expires_at === null
+            || $cloud->access_token_expires_at->isBefore(now())
         ) {
-            $response = Http::post($config->auth_url.'/v3/auth/tokens', [
+            $response = Http::post($cloud->auth_url.'/v3/auth/tokens', [
                 'auth' => [
                     'identity' => [
                         'methods' => ['password'],
                         'password' => [
                             'user' => [
-                                'domain' => ['name' => $config->auth_user_domain_name],
-                                'name' => $config->auth_username,
-                                'password' => $config->auth_password,
+                                'domain' => ['name' => $cloud->auth_user_domain_name],
+                                'name' => $cloud->auth_username,
+                                'password' => $cloud->auth_password,
                             ],
                         ],
                     ],
                     'scope' => [
-                        'project' => ['id' => $config->auth_project_id],
+                        'project' => ['id' => $cloud->auth_project_id],
                     ],
                 ],
             ]);
@@ -52,15 +56,13 @@ final class OpenstackService
                 throw new \RuntimeException('Failed to connect to OpenStack identity service: '.$response->body());
             }
 
-            $config->access_token = $response->header('X-Subject-Token');
-            $config->access_token_expires_at = Carbon::parse($response->json('token.expires_at'));
-            $config->endpoint_rating = $this->retrieveRatingEndpoint($config, $response->json('token.catalog'));
-            $config->save();
-
-            ray($config);
+            $cloud->access_token = $response->header('X-Subject-Token');
+            $cloud->access_token_expires_at = Carbon::parse($response->json('token.expires_at'));
+            $cloud->endpoint_rating = $this->retrieveRatingEndpoint($cloud, $response->json('token.catalog'));
+            $cloud->save();
         }
 
-        return $config->access_token;
+        return $cloud->access_token;
     }
 
     public function retrieveRatingEndpoint(OpenstackCloud $openstackCloud, array $catalog): string
@@ -85,33 +87,79 @@ final class OpenstackService
         return $url;
     }
 
-    public function getRatingsFor(?CarbonPeriod $period): array
+    public function getRatingsFor(?CarbonPeriod $period, ?User $user = null): array
     {
-        if (! self::isCloudConfigExistForAuth()) {
+        if (! self::isCloudConfigExistForAuth($user)) {
             throw new \RuntimeException('No OpenStack cloud configuration found for the user.');
         }
 
         /** @var OpenstackCloud $cloud */
-        $cloud = auth()->user()->openstackClouds->first();
+        $cloud = $user?->openstackClouds->first() ?? auth()->user()->openstackClouds->first();
 
-        $accessToken = $this->getAccessToken();
+        $accessToken = $this->getAccessToken($user);
         $response = Http::withHeaders([
             'X-Auth-Token' => $accessToken,
             'Content-Type' => 'application/json',
         ])
             ->acceptJson()
             ->get($cloud->endpoint_rating.'v1/storage/dataframes', [
-                'begin' => $period?->start()->toIso8601String() ?? now()->startOfMonth()->toIso8601String(),
-                'end' => $period?->end()->toIso8601String() ?? now()->subHour()->startOfHour()->toIso8601String(),
+                'begin' => $period?->start()?->toIso8601String() ?? now()->startOfMonth()->toIso8601String(),
+                'end' => $period?->end()?->toIso8601String() ?? now()->subHour()->startOfHour()->toIso8601String(),
             ]);
 
         if ($response->failed()) {
             throw new \RuntimeException('Failed to fetch ratings from OpenStack: '.$response->body());
         }
 
-        //        foreach ($response->json('dataframes') as $dataframe) {
-        //            //
-        //        }
+        foreach ($response->json('dataframes') as $dataframe) {
+            foreach ($dataframe['resources'] as $resource) {
+                $OSProject = OsProject::query()->firstOrCreate(
+                    [
+                        'project_identifier' => $resource['desc']['project_id'],
+                    ],
+                    [
+                        'project_identifier' => $resource['desc']['project_id'],
+                        'name' => $resource['desc']['project_id'],
+                        'openstack_cloud_id' => $cloud->id,
+                    ]
+                );
+
+                $OSResource = OsResource::query()->firstOrCreate(
+                    [
+                        'resource_identifier' => $resource['desc']['id'],
+                    ],
+                    [
+                        'resource_identifier' => $resource['desc']['id'],
+                        'name' => $resource['desc']['flavor_name'] ?? $resource['service'],
+                        'flavor_name' => $resource['desc']['flavor_name'] ?? null,
+                        'state' => $resource['desc']['state'] ?? null,
+                        'os_project_id' => $OSProject->id,
+                    ]
+                );
+
+                if (
+                    OsRating::query()
+                        ->where('begin', '=', Carbon::parse($dataframe['begin']))
+                        ->where('end', '=', Carbon::parse($dataframe['end']))
+                        ->where('service', '=', $resource['service'])
+                        ->where('os_resource_id', '=', $OSResource->id)
+                        ->exists()
+                ) {
+                    continue; // Skip if the rating already exists
+                }
+
+                $OSRating = new OsRating;
+                $OSRating->fill([
+                    'rating' => (float) $resource['rating'],
+                    'volume' => (float) $resource['volume'],
+                    'begin' => Carbon::parse($dataframe['begin']),
+                    'end' => Carbon::parse($dataframe['end']),
+                    'service' => $resource['service'],
+                    'os_resource_id' => $OSResource->id,
+                ]);
+                $OSRating->save();
+            }
+        }
 
         return $response->json('dataframes', []);
     }
